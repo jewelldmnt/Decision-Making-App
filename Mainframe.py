ca91f58a19853a60f2e63f423d79ff97ca2862ba
jewelldmnt <jewellanneddiamante@iskolarngbayan.pup.edu.ph>
2022-12-15 13:53:25 +0800

    pushed latest version: Dec 15 at 1:53 pm.

--- Mainframe.py
+++ Mainframe.py
@@ -69,6 +69,60 @@ class MainFrame(Tk):
                     "Technical": 0}
         }
 
+        # description for skills
+        self.skills_description = {
+            "STEM": {"Problem-Solving": "You have the ability to recognize the problem by thinking \n"
+                                        "outside the box and come up with different alternative \n"
+                                        "approaches that would help you to take the appropriate \n"
+                                        "course of action in a certain problem.",
+
+                     "Math-Science": "You want being challenged by solving different math equations \n"
+                                     "and it is also fundamental in understanding science which you \n"
+                                     "also enjoy.",
+
+                     "Innovative": "You have knowledge and abilities you use to create and adapt to \n"
+                                   "change, this will allow you to use your existing knowledge to \n"
+                                   "discover original ideas that benefit you and your team."},
+
+            "ABM": {"Business Communication": "You can build relationships by carefully listening \n"
+                                              "to others, has a good judgement, and confident to \n"
+                                              "speak clearly with your audience comfortably. ",
+                    "Financial Management": " You are able to manage your perosonal finances and \n"
+                                            "able to apply neccesary skills to spend your own money \n"
+                                            "wisely.",
+                    "Business": "You can think of opportunities to earn and able to \n"
+                                "lead a group that has the same goals."},
+
+            "HUMSS": {"Communication": "You are confident to speak out your thoughts, and you \n"
+                                       "can easily communicate and resonate with people easily. ",
+                      "Creativity": "With the constant change in circumstances, you frequently \n"
+                                    "come up with fresh (creative) solutions to specific difficulties, \n"
+                                    "thus creatively expressing yourself in that way. ",
+                      "Social and Cultural Awareness": "You have sensitivity and respect for differences among \n"
+                                                       "people you interact with, thus, being willing to objectively \n"
+                                                       "examine the values, beliefs, traditions, and perceptions \n"
+                                                       "within your own and other cultures."},
+
+            "ICT": {"Innovative": "You have the capacity to think critically about \n"
+                                  "the best solution to a given problem and picture \n"
+                                  "situations from several points of view. ",
+                    "Computer": "You can utilize computers and other associated \n"
+                                "technologies because you have the knowledge and \n"
+                                "skills necessary.",
+                    "Marketing and Video-Editing": "You can creatively create videos that entertain \n"
+                                                   "audiences or have an interest to make videos that \n"
+                                                   "captures people's attention."},
+
+            "GAS": {"Adaptability": "You seem uncertain and confused on what specific \n"
+                                    "path you would take and you still want to weigh \n"
+                                    "your options.",
+                    "Presentation": "You seem uncertain and confused on what specific \n"
+                                    "path you would take and you still want to weigh \n"
+                                    "your options.",
+                    "Technical": "You are flexible enough to take any strand in \n"
+                                 "Senior High School because of your eagerness to \n"
+                                 "learn different subjects."}
+        }
         self.result_strand = ''
         self.result_skills = ''
         # the user's name
@@ -77,7 +131,7 @@ class MainFrame(Tk):
         self.strand = ''
 
         # calling the first screen
-        self.show_frame("ABM")
+        self.show_frame("StartPage")
 
     def __setitem__(self, key, value):
         self.strands_ratings[key] += value
@@ -92,20 +146,20 @@ class MainFrame(Tk):
         skills_copy = self.skills[self.result_strand]
 
         # getting the skills with the highest rating
-        self.result_skills = [key for key, value in skills_copy.items() if value == max(skills_copy.values())]
+        self.result_skills = max(skills_copy, key=skills_copy.get)
 
     def graph(self):
         colors = ['green', 'blue', 'purple', 'teal', 'orange']
         ratings = list(self.strands_ratings.values())
         x_axis = ["STEM", "ABM", "HUMSS", "ICT", "GAS"]
         y_axis = ratings
-        plt.ylim(-15, 70)
+        plt.ylim(-20, 70)
         plt.bar(x_axis, y_axis, color=colors)
         for idx, val in enumerate(ratings):
             plt.text(idx, val + 1, val, ha="center")
 
         plt.title("StrGuide Result")
-        plt.text(2, -8, "Dito ilalagay yung skills description. \nTriny ko ilabas sa box kaso hindi siya nababasa", ha="center")
+        plt.text(2, -18, self.skills_description[self.result_strand][self.result_skills], ha="center")
         plt.xlabel("Strands", fontsize=8)
         plt.ylabel("Ratings", fontsize=8)
         plt.show()
